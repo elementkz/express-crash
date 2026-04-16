59bd657cb8fc407a7aee2a516e3d77a90bc70e04
elementkz <adamroitub7@gmail.com>
2026-04-16 13:44:41 +0300

    made static folder

--- server.js
+++ server.js
@@ -4,13 +4,7 @@ import path from "path";
 import { __dirname } from "./lib.js";
 
 const app = express();
-
-app.get("/", (req, res) => {
-	res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-app.get("/about", (req, res) => {
-	res.sendFile(path.join(__dirname, "public", "about.html"));
-});
+// Setup static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(8000, (_) => console.log(`server is running on port ${8000}`));
