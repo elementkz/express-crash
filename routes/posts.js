f72ef913cf00afcbf01470ffa5fd9d111c5d8cbe
elementkz <adamroitub7@gmail.com>
2026-04-17 14:46:52 +0300

    added POST to add posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,6 @@
 import express from "express";
 
 const router = express.Router();
-import path from "path";
-
-import { __dirname } from "../lib.js";
 
 let posts = [
 	{ id: 1, title: "Post #1" },
@@ -29,6 +26,20 @@ router.get("/:id", (req, res) => {
 			msg: `A post with with the id of ${id} does not exist`,
 		});
 	return res.status(200).json(posts.filter((posts) => posts.id === id));
+});
+
+// Create new post
+router.post("/", (req, res) => {
+	const newPost = {
+		id: posts.length + 1,
+		title: req.body.title,
+	};
+
+	if (!newPost.title)
+		return res.status(400).json({ msg: "Please include a title" });
+
+	posts.push(newPost);
+	return res.status(201).json(posts);
 });
 
 export default router;
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const port = process.env.PORT || 8000;
 
 import express from "express";
-
 import posts from "./routes/posts.js";
-
 const app = express();
-// Setup static folder
-// app.use(express.static(path.join(__dirname, "public")));
+
+// bodyparser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Routes
 app.use("/api/posts", posts);
